1cc2467738024ca3aa6616eba3cc8882f762c58b
motorolla29 <eutyou@gmail.com>
2023-12-21 17:59:04 +0300

    projects data filled

--- src/data/data.js
+++ src/data/data.js
@@ -1,16 +1,135 @@
 export const PROJECTS_DATA = [
   {
     id: 1,
-    title: 'Pied Piper',
+    title: 'M29',
     subtitle:
-      'A simple landing web page, created on a training course, stylized in pure CSS with simple animations.',
-    githubLink: 'https://github.com/motorolla29/PiedPiper',
-    demoLink: 'https://pied-piper-m29.vercel.app/',
-    skills: ['html', 'css'],
-    preview: 'pied-piper',
+      'Personal website - portfolio with 4 pages, created using react. Implemented home page with interactive canvas and contact form on formspree. GreenSock animation platform and three.js also were used here.',
+    githubLink: '/',
+    demoLink: '/',
+    skills: [
+      'html',
+      'css',
+      'javascript',
+      'git',
+      'npm',
+      'react',
+      'gsap',
+      'three',
+    ],
+    preview: 'm29',
   },
   {
     id: 2,
+    title: 'Six Cities',
+    subtitle:
+      'Service for booking accommodation in 6 cities using the React - Redux bundle, covered with Jest tests with an implemented leaflet map and authorization with the ability to submit a review for the current property.',
+    githubLink: 'https://github.com/motorolla29/SIX-CITIES',
+    demoLink: 'https://six-cities-m29.vercel.app/',
+    skills: ['git', 'npm', 'webpack', 'babel', 'react', 'redux', 'jest'],
+    preview: 'six-cities',
+  },
+  {
+    id: 3,
+    title: 'Game of Thrones',
+    subtitle:
+      'Experience on using React class components with data received from the API based on https://anapioficeandfire.com/. Writing simple Unit tests with Jest also.',
+    githubLink: 'https://github.com/motorolla29/Game-Of-Thrones-DB/',
+    demoLink: 'https://game-of-thrones-db-m29.vercel.app/',
+    skills: ['html', 'css', 'javascript', 'git', 'npm', 'react', 'jest'],
+    preview: 'got-db',
+  },
+  {
+    id: 4,
+    title: 'Resto',
+    subtitle:
+      'Restaurant menu written in React and application state management using Redux. Routing between main page and cart page. Fetching data from JSON-server based on Vercel.',
+    githubLink: 'https://github.com/motorolla29/Resto',
+    demoLink: 'https://resto-m29.vercel.app/',
+    skills: ['git', 'npm', 'react', 'redux'],
+    preview: 'resto',
+  },
+  {
+    id: 5,
+    title: 'Star DB',
+    subtitle:
+      'Work aimed at studying react, class and functional components, hooks, routing, HOCs, react context, compose function, data fetch services with API based on https://swapi.dev/api and proptypes.',
+    githubLink: 'https://github.com/motorolla29/star-db',
+    demoLink: 'https://star-db-m29.vercel.app/',
+    skills: ['git', 'npm', 'react'],
+    preview: 'star-db',
+  },
+  {
+    id: 6,
+    title: 'Todo App',
+    subtitle:
+      'A notes application written on React with create-react-app library, you can delete and add notes and mark completed ones.',
+    githubLink: 'https://github.com/motorolla29/todo-app',
+    demoLink: 'https://todo-app-m29.vercel.app/',
+    skills: ['git', 'npm', 'react'],
+    preview: 'todo-app',
+  },
+  {
+    id: 7,
+    title: 'Draft Note App',
+    subtitle:
+      'Another notes app, this time you can like your notes, add to favorites and filter a notes by liked.',
+    githubLink: 'https://github.com/motorolla29/Draft-Note-App',
+    demoLink: 'https://draft-note-app-m29.vercel.app/',
+    skills: ['git', 'npm', 'react'],
+    preview: 'draft-note',
+  },
+  {
+    id: 8,
+    title: 'Pixel Hunter',
+    subtitle:
+      'A game for guessing photos or photorealistic drawings. MVP pattern using OOP in JS. Fetching data with JSON-server. Unit-tests with "chai". Building with GULP.',
+    githubLink: 'https://github.com/motorolla29/pixel-hunter',
+    demoLink: 'https://pixel-hunter-m29.vercel.app/',
+    skills: ['html', 'css', 'javascript', 'git', 'npm', 'gulp', 'sass'],
+    preview: 'pixel-hunter',
+  },
+  {
+    id: 9,
+    title: 'Keksobooking',
+    subtitle:
+      'The project was created during the process of learning native JS, receiving data using XMLHttpRequest, sorting cards, form validation, drag and drop are implemented in this project.',
+    githubLink: 'https://github.com/motorolla29/keksobooking',
+    demoLink: 'https://keksobooking-m29.vercel.app/',
+    skills: ['html', 'css', 'javascript', 'git'],
+    preview: 'keksobooking',
+  },
+  {
+    id: 10,
+    title: 'Diet Food',
+    subtitle:
+      'Training in native JS, implementation of such things as a slider, timer, modal window, calculator and countdown to the end of the promo. Json-server used for training data fetching. Compiled with webpack.',
+    githubLink: 'https://github.com/motorolla29/food',
+    demoLink: 'https://diet-food-m29.vercel.app/',
+    skills: ['html', 'css', 'javascript', 'npm', 'git', 'webpack'],
+    preview: 'diet-food',
+  },
+  {
+    id: 11,
+    title: 'Movie DB',
+    subtitle:
+      'First experience of creating a website using native JavaScript, a project with films, styles on the Sass preprocessor were used in the work',
+    githubLink: 'https://github.com/motorolla29/Movie-DB',
+    demoLink: 'https://movie-db-m29.vercel.app/',
+    skills: ['html', 'css', 'javascript', 'sass'],
+    preview: 'movie-db',
+  },
+  {
+    id: 12,
+    title: 'Rain of Vegetables',
+    subtitle:
+      'Just an animation using vector graphics canvas, written in prototype-based programming of OOP in JS, with interactive addition of cheese in the rain by user click =)',
+    githubLink: 'https://github.com/motorolla29/Vegetable-Rain',
+    demoLink: 'https://rain-of-vegetables.vercel.app/',
+    skills: ['javascript'],
+    preview: 'veg-rain',
+  },
+  {
+    id: 13,
     title: 'Uber Eats',
     subtitle:
       'Two stylized with CSS web pages of Uber Eats app, where ara main page and the Pushkin restaurant page, which will open by clicking on any card.',
@@ -21,23 +140,13 @@ export const PROJECTS_DATA = [
     preview: 'uber-eats',
   },
   {
-    id: 3,
-    title: 'Diet Food',
+    id: 14,
+    title: 'Pied Piper',
     subtitle:
-      'First experience of creating a website using native JavaScript, implementation of such basic things as a slider, timer, modal window, calculator and countdown to the end of the promotion. Json-server was used to train how to work with data received from the server, and webpack was used to assemble the project.',
-    githubLink: 'https://github.com/motorolla29/food',
-    demoLink: 'https://diet-food-m29.vercel.app/',
-    skills: ['html', 'css', 'javascript', 'json-server', 'webpack'],
-    preview: 'diet-food',
-  },
-  {
-    id: 4,
-    title: 'Game of Thrones',
-    subtitle:
-      'Experience in a training course on using React class components with data received from the API based on https://anapioficeandfire.com/.',
-    githubLink: 'https://github.com/motorolla29/Game-Of-Thrones-DB/',
-    demoLink: 'https://game-of-thrones-db-m29.vercel.app/',
-    skills: ['html', 'css', 'javascript', 'react'],
-    preview: 'got-db',
+      'A simple landing web page, created on a training course, stylized in pure CSS with simple animations.',
+    githubLink: 'https://github.com/motorolla29/PiedPiper',
+    demoLink: 'https://pied-piper-m29.vercel.app/',
+    skills: ['html', 'css'],
+    preview: 'pied-piper',
   },
 ];
