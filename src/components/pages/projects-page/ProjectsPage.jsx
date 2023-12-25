83228f8dd65861e9f8fd20aaa8fd5cd6d3bb0634
motorolla29 <eutyou@gmail.com>
2023-12-25 16:56:27 +0300

    swiping projects is prohibited during zoom

--- src/components/pages/projects-page/ProjectsPage.jsx
+++ src/components/pages/projects-page/ProjectsPage.jsx
@@ -1,5 +1,5 @@
 import { useParams, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect } from 'react';
 
 import Pagination from '../../pagination/Pagination';
 import Projects from '../../projects/Projects';
@@ -13,53 +13,64 @@ const ProjectsPage = () => {
 
   let currentProject = PROJECTS_DATA[id - 1] || PROJECTS_DATA[0];
 
-  const [touchstartX, setTouchstartX] = useState(0);
-  const [touchendX, setTouchendX] = useState(0);
-  const [touchstartY, setTouchstartY] = useState(0);
-  const [touchendY, setTouchendY] = useState(0);
+  useEffect(() => {
+    let touchstartX = 0;
+    let touchendX = 0;
+    let touchstartY = 0;
+    let touchendY = 0;
 
-  function handleGesure() {
-    if (
-      touchendX < touchstartX - 50 &&
-      touchendY - touchstartY < 50 &&
-      touchendY - touchstartY > -50
-    ) {
-      if (!id) {
-        return navigate(`/projects/2`);
-      } else {
-        return id < PROJECTS_DATA.length ? navigate(`/projects/${++id}`) : null;
+    function handleGesure() {
+      if (
+        !(window.visualViewport.scale > 1) &&
+        touchendX < touchstartX - 25 &&
+        touchendY - touchstartY < 25 &&
+        touchendY - touchstartY > -25
+      ) {
+        if (!id) {
+          return navigate(`/projects/2`);
+        } else {
+          return id < PROJECTS_DATA.length
+            ? navigate(`/projects/${++id}`)
+            : null;
+        }
+      }
+      if (
+        !(window.visualViewport.scale > 1) &&
+        touchendX > touchstartX + 25 &&
+        touchendY - touchstartY < 25 &&
+        touchendY - touchstartY > -25
+      ) {
+        return id > 1 ? navigate(`/projects/${--id}`) : null;
       }
     }
-    if (
-      touchendX > touchstartX + 50 &&
-      touchendY - touchstartY < 50 &&
-      touchendY - touchstartY > -50
-    ) {
-      return id > 1 ? navigate(`/projects/${--id}`) : null;
-    }
-  }
+    const gesuredZone = document.querySelector('.projects_page_container');
 
-  const onTouchstartHandler = (e) => {
-    if (e.targetTouches.length === 1) {
-      setTouchstartX(e.targetTouches[0].screenX);
-      setTouchstartY(e.targetTouches[0].screenY);
-    }
-  };
+    const onTouchstartHandler = (e) => {
+      if (e.targetTouches.length === 1) {
+        touchstartX = e.targetTouches[0].screenX;
+        touchstartY = e.targetTouches[0].screenY;
+      }
+    };
 
-  const onTouchendHandler = (e) => {
-    if (e.changedTouches.length === 1) {
-      setTouchendX(e.changedTouches[0].screenX);
-      setTouchendY(e.changedTouches[0].screenY);
-      handleGesure();
-    }
-  };
+    const onTouchendHandler = (e) => {
+      if (e.changedTouches.length === 1) {
+        touchendX = e.changedTouches[0].screenX;
+        touchendY = e.changedTouches[0].screenY;
+        handleGesure();
+      }
+    };
+
+    gesuredZone.addEventListener('touchstart', onTouchstartHandler, false);
+    gesuredZone.addEventListener('touchend', onTouchendHandler, false);
+
+    return () => {
+      gesuredZone.removeEventListener('touchstart', onTouchstartHandler);
+      gesuredZone.removeEventListener('touchend', onTouchendHandler);
+    };
+  });
 
   return (
-    <div
-      onTouchStart={onTouchstartHandler}
-      onTouchEnd={onTouchendHandler}
-      className="projects_page_container"
-    >
+    <div className="projects_page_container">
       <Projects project={currentProject} />
       <Pagination data={PROJECTS_DATA} currentProject={currentProject} />
     </div>
